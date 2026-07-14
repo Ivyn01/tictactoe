0895e113b155da712bd2e24eefa21416efa3126c
4m4y <amay.stu@gmail.com>
2026-07-14 16:10:52 +0200

    Added boilerplate

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+
+function Main () {
+    return (
+        <div>
+            3
+        </div>
+    )
+}
+
+createRoot(document.querySelector('#root')).render(< Main />)
