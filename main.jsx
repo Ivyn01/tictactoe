64d2c215cf7e170d25afbd85c523f6d81f7249aa
4m4y <amay.stu@gmail.com>
2026-07-16 15:17:37 +0200

    Rendering of board and XO

--- main.jsx
+++ main.jsx
@@ -1,11 +1,36 @@
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
 
-function Main () {
-    return (
-        <div>
-            3
+function Main() {
+  const [toggle, setToggle] = useState(true);
+  const [arr1, setArr1] = useState(["-", "-", "-"]);
+  const [arr2, setArr2] = useState(["-", "-", "-"]);
+  const [arr3, setArr3] = useState(["-", "-", "-"]);
+  const mainArr = [arr1, arr2, arr3];
+  function handle(i,j) {
+    let btn = document.getElementById(`${i}-${j}`)
+    if (btn.innerHTML === 'X' || btn.innerHTML === 'O') {
+      return
+    }
+    btn.innerHTML = toggle ? 'X' : 'O'
+    setToggle(!toggle)
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center my-50">
+      {mainArr.map((i, row) => (
+        <div key={row}>
+          {i.map((j, column) => (
+            <button className="h-25 w-25 border-2 hover:bg-teal-200" key={column} id={`${row}-${column}`} onClick={() => {
+              handle(row,column)
+            }}>
+              {j}
+            </button>
+          ))}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-createRoot(document.querySelector('#root')).render(< Main />)
+createRoot(document.querySelector("#root")).render(<Main />);
